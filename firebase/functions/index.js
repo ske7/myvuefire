226290d6813bfaa3c3bbf7a4c9f83253aa9498e5
ske7 <ske777@gmail.com>
2018-06-12 00:18:27 +0300

    add new firebase cloud function "modifyLockOfUser"

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,11 @@
+// Packages
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+
+// Init
 admin.initializeApp(functions.config().firebase);
 
-
+// Functions
 function deleteCollection(collectionPath, batchSize) {
 	let collectionRef = admin.firestore().collection(collectionPath);
 	let query = collectionRef.limit(batchSize);
@@ -13,7 +16,8 @@ function deleteCollection(collectionPath, batchSize) {
 }
 
 function deleteQueryBatch(query, batchSize, resolve, reject) {
-	query.get()
+	query
+		.get()
 		.then((snapshot) => {
 			if (snapshot.size === 0) {
 				return 0;
@@ -31,20 +35,44 @@ function deleteQueryBatch(query, batchSize, resolve, reject) {
 		.then((numDeleted) => {
 			if (numDeleted === 0) {
 				resolve();
-				return;
+				return true;
+			} else {
+				process.nextTick(() => {
+					deleteQueryBatch(query, batchSize, resolve, reject);
+				});
+				return false;
 			}
-
-			process.nextTick(() => {
-					 deleteQueryBatch(query, batchSize, resolve, reject);
-			});
 		})
 		.catch(reject);
 }
 
-exports.deleteProfile = functions.auth.user().onDelete(user => {
-	deleteCollection(`/users/${user.uid}/logins`, 5)
+const deleteProfile = functions.auth.user().onDelete((user) => {
+	return deleteCollection(`/users/${user.uid}/logins`, 5)
 		.then(() => {
 			return admin.firestore().collection("users").doc(user.uid).delete();
 		})
 		.catch();
 });
+
+const modifyLockOfUser = functions.https.onCall((data, context) => {
+	return admin
+		.auth()
+		.updateUser(data.uid, {
+			disabled: data.disabled
+		})
+		.then((userRecord) => {
+			const lockedStr = data.disabled ? "locked" : "unlocked";
+			// console.log(`Successfully ${lockedStr} the user`, userRecord.toJSON());
+			return { done: true, message: `Successfully ${lockedStr} the user` };
+		})
+		.catch((error) => {
+			console.log("Error updating user:", error);
+			return error;
+		});
+});
+
+// Exports
+module.exports = {
+	deleteProfile,
+	modifyLockOfUser
+};
